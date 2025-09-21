f9b67334fc8c1b0fbe49aeb27fa0e2ba40e67f9d
Nicolas Martinez V <nmartinezv@icloud.com>
2025-09-21 17:32:06 -0300

    feat: add daisyui dependencie

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,14 @@
+import tailwindcss from "@tailwindcss/vite";
+import tsconfigPaths from 'vite-tsconfig-paths'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image']
+  devtools: { enabled: false },
+  modules: ['@nuxt/content', '@nuxt/eslint', '@nuxt/image'],
+  plugins: [],
+  vite: {
+    plugins: [tailwindcss(), tsconfigPaths()],
+  },
+  css: ["~/assets/app.css"],
 })
